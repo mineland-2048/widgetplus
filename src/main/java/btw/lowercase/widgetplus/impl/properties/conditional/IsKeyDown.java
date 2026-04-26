0203b9a13f5f1999cc4827250b3179a56d3f099e
lowercasebtw <jejojora2@gmail.com>
2026-04-26 18:38:46 -0500

    Key protection

--- src/main/java/btw/lowercase/widgetplus/impl/properties/conditional/IsKeyDown.java
+++ src/main/java/btw/lowercase/widgetplus/impl/properties/conditional/IsKeyDown.java
@@ -3,20 +3,28 @@ package btw.lowercase.widgetplus.impl.properties.conditional;
 import btw.lowercase.widgetplus.impl.property.ConditionalWidgetProperty;
 import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.AbstractWidget;
 
-public record IsKeyDown(String key) implements ConditionalWidgetProperty {
+public record IsKeyDown(InputConstants.Key key) implements ConditionalWidgetProperty {
+    private static final Codec<InputConstants.Key> KEY_CODEC = Codec.STRING.comapFlatMap(key -> {
+        try {
+            return DataResult.success(InputConstants.getKey(key));
+        } catch (final Throwable throwable) {
+            return DataResult.error(() -> "Invalid key: " + key);
+        }
+    }, InputConstants.Key::getName);
+
     public static final MapCodec<IsKeyDown> MAP_CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-            Codec.STRING.fieldOf("key").forGetter(IsKeyDown::key)
+            KEY_CODEC.fieldOf("key").forGetter(IsKeyDown::key)
     ).apply(instance, IsKeyDown::new));
 
     @Override
     public boolean get(final AbstractWidget widget) {
-        final InputConstants.Key key = InputConstants.getKey(this.key);
-        return InputConstants.isKeyDown(Minecraft.getInstance().getWindow(), key.getValue());
+        return InputConstants.isKeyDown(Minecraft.getInstance().getWindow(), this.key.getValue());
     }
 
     @Override
